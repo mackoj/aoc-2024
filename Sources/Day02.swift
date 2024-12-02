ee8aea84007a73bc3327afe52d347f0c058ba8f4
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-12-03 00:35:10 +0100

    Day 2 cleaned

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -3,57 +3,47 @@ import Parsing
 
 struct Day02: AdventDay {
   var data: String
+  let reports: [Report]
   
-  struct Report {
-    let levels: [Int]
-    
-    init(levels: [Int]) {
-      self.levels = levels
+  init(data: String) {
+    self.data = data
+    do {
+      self.reports = try Day02.parse(data)
+    } catch {
+      fatalError(error.localizedDescription)
     }
-  }
-
-  struct ReportParser: Parser {
-    var body: some Parser<Substring, Report> {
-      Many {
-        Int.parser()
-      } separator: {
-        " "
-      }.map(Report.init(levels:))
-    }
-  }
-
-  struct ReportsParser: Parser {
-    var body: some Parser<Substring, [Report]> {
-      Many {
-        ReportParser()
-      } separator: {
-        "\n"
-      }
-    }
-  }
-
-  func parse() throws -> [Report] {
-    try ReportsParser().parse(data)
   }
   
+  func part1() -> Any {
+    return reports.reduce(into: 0) { partialResult, report in
+      if isSafeReport(report.levels) { partialResult += 1 }
+    }
+  }
+  
+  func part2() -> Any {
+    return reports.reduce(into: 0) { partialResult, report in
+      if isSafeWithOneRemoval(report.levels) { partialResult += 1 }
+    }
+  }
+  
+  // MARK: - Day 2
   func isSafeReport(_ levels: [Int]) -> Bool {
     guard levels.count >= 2 else { return false }
-    let trend = levels[1] - levels[0]
-    if trend == 0 { return false } // flat
-    let isIncreasing = trend > 0
-    let isDecreasing = trend < 0
+    guard let trend = Trend(levels[1] - levels[0]) else { return false }
     for i in 1..<levels.count {
       let diff = levels[i] - levels[i - 1]
-      let diffTrendIsIncreasing = diff >= 0
-      let diffTrendIsDecreasing = diff <= 0
+      guard let diffTrend = Trend(diff) else { return false }
       let absDiff = abs(diff)
-      if absDiff < 1 || absDiff > 3 { return false }
-      if isIncreasing && diffTrendIsIncreasing { return false }
-      if isDecreasing && diffTrendIsDecreasing { return false }
+      if absDiff < 1 || absDiff > 3 { return false } // main rule
+      switch (trend, diffTrend) {
+        case (.increasing, .decreasing): return false
+        case (.decreasing, .increasing): return false
+        default: break
+      }
     }
     return true
   }
-
+  
   func isSafeWithOneRemoval(_ levels: [Int]) -> Bool {
     // normal case
     if isSafeReport(levels) {
@@ -65,24 +55,59 @@ struct Day02: AdventDay {
       var modifiedLevels = levels
       modifiedLevels.remove(at: i)
       if isSafeReport(modifiedLevels) {
-          return true
+        return true
       }
     }
-
+    
     return false
   }
   
-  func part1() -> Any {
-    let reports = (try? parse()) ?? []
-    return reports.reduce(into: 0) { partialResult, report in
-      if isSafeReport(report.levels) { partialResult += 1 }
+  // MARK: - Models
+  struct Report {
+    let levels: [Int]
+    
+    init(levels: [Int]) {
+      self.levels = levels
     }
   }
   
-  func part2() -> Any {
-    let reports = (try? parse()) ?? []
-    return reports.reduce(into: 0) { partialResult, report in
-      if isSafeWithOneRemoval(report.levels) { partialResult += 1 }
+  enum Trend {
+    case increasing
+    case decreasing
+    
+    init?(_ trend: Int) {
+      if trend > 0 {
+        self = .increasing
+      } else if trend < 0 {
+        self = .decreasing
+      } else {
+        return nil
+      }
     }
+  }
+  
+  // MARK: Parsing
+  struct ReportParser: Parser {
+    var body: some Parser<Substring, Report> {
+      Many {
+        Int.parser()
+      } separator: {
+        " "
+      }.map(Report.init(levels:))
+    }
+  }
+  
+  struct ReportsParser: Parser {
+    var body: some Parser<Substring, [Report]> {
+      Many {
+        ReportParser()
+      } separator: {
+        "\n"
+      }
+    }
+  }
+  
+  static func parse(_ data: String) throws -> [Report] {
+    try ReportsParser().parse(data)
   }
 }
