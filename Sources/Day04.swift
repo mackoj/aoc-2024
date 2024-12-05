8dcb4bc7dd83b5d32c04bcf1047bade578f5024a
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-12-05 14:02:32 +0100

    day 4

--- Sources/Day04.swift
+++ Sources/Day04.swift
@@ -2,19 +2,22 @@ import Foundation
 
 struct Day04: AdventDay {
   var data: String
-
+  
   init(data: String) {
     self.data = data
   }
   
   func extractVerticalInput(_ horizontalInput: [String]) -> [String] {
-    let charCount = horizontalInput[0].count
+    guard let firstLine = horizontalInput.first, !firstLine.isEmpty else { return [] }
+    let charCount = firstLine.count
     var result = [String](repeating: "", count: charCount)
     
     for line in horizontalInput where !line.isEmpty {
-        for (index, char) in line.enumerated() {
-            result[index].append(char)
+      for (index, char) in line.enumerated() {
+        if index < charCount {
+          result[index].append(char)
         }
+      }
     }
     
     return result
@@ -22,46 +25,50 @@ struct Day04: AdventDay {
   
   func extractDiagonalInput(_ horizontalInput: [String]) -> [String] {
     let rowCount = horizontalInput.count
-    let colCount = horizontalInput[0].count
+    guard rowCount > 0, let firstLine = horizontalInput.first else { return [] }
+    let colCount = firstLine.count
     var diagonals: [String] = []
-
+    
     // Collect diagonals from top-left to bottom-right
     for start in 0..<(rowCount + colCount - 1) {
-        var diagonalTLBR = ""
-        for i in 0..<rowCount {
-            let j = start - i
-            if j >= 0 && j < colCount {
-                diagonalTLBR.append(horizontalInput[i][horizontalInput[i].index(horizontalInput[i].startIndex, offsetBy: j)])
-            }
+      var diagonalTLBR = ""
+      for i in 0..<rowCount {
+        let j = start - i
+        if j >= 0, j < colCount, i < rowCount, !horizontalInput[i].isEmpty {
+          diagonalTLBR.append(horizontalInput[i][horizontalInput[i].index(horizontalInput[i].startIndex, offsetBy: j)])
         }
-        if !diagonalTLBR.isEmpty {
-            diagonals.append(diagonalTLBR)
-        }
+      }
+      if !diagonalTLBR.isEmpty {
+        diagonals.append(diagonalTLBR)
+      }
     }
-
+    
     // Collect diagonals from top-right to bottom-left
     for start in 0..<(rowCount + colCount - 1) {
-        var diagonalTRBL = ""
-        for i in 0..<rowCount {
-            let j = colCount - 1 - (start - i)
-            if j >= 0 && j < colCount {
-                diagonalTRBL.append(horizontalInput[i][horizontalInput[i].index(horizontalInput[i].startIndex, offsetBy: j)])
-            }
+      var diagonalTRBL = ""
+      for i in 0..<rowCount {
+        let j = colCount - 1 - (start - i)
+        if j >= 0, j < colCount, i < rowCount, !horizontalInput[i].isEmpty {
+          diagonalTRBL.append(horizontalInput[i][horizontalInput[i].index(horizontalInput[i].startIndex, offsetBy: j)])
         }
-        if !diagonalTRBL.isEmpty {
-            diagonals.append(diagonalTRBL)
-        }
+      }
+      if !diagonalTRBL.isEmpty {
+        diagonals.append(diagonalTRBL)
+      }
     }
-
+    
     return diagonals
   }
-
   
   func countForWord(_ word: String, _ horizontalInput: [String], _ verticalInput: [String], _ diagonalInput: [String]) -> Int {
-    let horizontalCount = horizontalInput.filter { $0.contains(word) }.count
-    let verticalCount = verticalInput.filter { $0.contains(word) }.count
-    let diagonalCount = diagonalInput.filter { $0.contains(word) }.count
-    return horizontalCount + verticalCount + diagonalCount
+    do {
+      let horizontalCount = try horizontalInput.reduce(into: 0) { $0 += $1.matches(of: try Regex(word)).count }
+      let verticalCount = try verticalInput.reduce(into: 0) { $0 += $1.matches(of: try Regex(word)).count }
+      let diagonalCount = try diagonalInput.reduce(into: 0) { $0 += $1.matches(of: try Regex(word)).count }
+      return horizontalCount + verticalCount + diagonalCount
+    } catch {
+      fatalError(error.localizedDescription)
+    }
   }
   
   func part1() -> Any {
@@ -80,8 +87,31 @@ struct Day04: AdventDay {
       diagonalInput
     )
   }
-
+  
   func part2() -> Any {
-    return 0
+    let grid = data
+      .components(separatedBy: .newlines)
+      .reduce(
+      into: [[Character]]()
+    ) { partialResult, sb in
+      partialResult.append(Array(sb))
+    }
+    var counter = 0
+    for (line, row) in grid.enumerated() where line > 0 && line < (grid.count - 2) {
+      for (column, character) in row.enumerated() where column > 0 && column < (row.count - 1) {
+        if character == "A" {
+          let topLeft = grid[line - 1][column - 1]
+          let topRight = grid[line - 1][column + 1]
+          let bottomLeft = grid[line + 1][column - 1]
+          let bottomRight = grid[line + 1][column + 1]
+          let diagonalL = (topLeft == "M" && bottomRight == "S") || (topLeft == "S" && bottomRight == "M")
+          let diagonalR = (topRight == "M" && bottomLeft == "S") || (topRight == "S" && bottomLeft == "M")
+          if diagonalL && diagonalR {
+            counter += 1
+          }
+        }
+      }
+    }
+    return counter
   }
 }
--- Tests/Day04.swift
+++ Tests/Day04.swift
@@ -20,7 +20,18 @@ MXMXAXMASX
   }
 
   @Test func testPart2() async throws {
-    let challenge = Day04(data: "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
-    #expect(String(describing: challenge.part2()) == "48")
+    let challenge = Day04(data: """
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+""")
+    #expect(String(describing: challenge.part2()) == "9")
   }
 }
